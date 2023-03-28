b2b5a8582ad112a831c17e93b1ac4636de920754
Trem de Programador <rosjdr@gmail.com>
2023-03-27 22:37:21 -0300

    Solução final do primeiro exercício do capítulo

--- java/capitulo3/src/br/com/tremdeprogramador/exe1/Main.java
+++ java/capitulo3/src/br/com/tremdeprogramador/exe1/Main.java
@@ -5,14 +5,13 @@ import java.util.Scanner;
 public class Main {
 	
 	public static void main(String args[]) {
-		int n1, n2, n3, n4, soma;
+		int n1, n2, resultado;
 		Scanner tec = new Scanner(System.in);
+		
 		n1 = tec.nextInt();
 		n2 = tec.nextInt();
-		n3 = tec.nextInt();
-		n4 = tec.nextInt();
-		soma = n1+n2+n3+n4;
-		System.out.println("Soma = " + soma);
+		resultado = n1-n2;
+		System.out.println("Subtração = " + resultado);
 	}
 
 }
