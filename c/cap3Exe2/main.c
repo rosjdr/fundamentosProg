1b81f2f327e40c6cb2ff06b79246812f07f51226
Trem de Programador <rosjdr@gmail.com>
2023-03-27 23:05:56 -0300

    C - Capítulo 3 - Exercício 2

---
+++ c/cap3Exe2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int n1, n2, n3, mult;
+
+    scanf("%d", &n1);
+    scanf("%d", &n2);
+    scanf("%d", &n3);
+
+    mult = n1*n2*n3;
+
+    printf("Mult = %d", mult);
+
+    return 0;
+}
