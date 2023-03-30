39e9fe24d6ed8ac0bc7bfb7877205097a670a886
Trem de Programador <rosjdr@gmail.com>
2023-03-30 16:05:42 -0300

    Refatoração do arquivo main.c

--- c/cap3Exe1/main.c
+++ c/cap3Exe1/main.c
@@ -3,12 +3,14 @@
 int main(){
     int n1, n2, sub;
 
+    printf("%s", "Informe o valor de n1: ");
     scanf("%d", &n1);
+    printf("%s", "Informe o valor de n2: ");
     scanf("%d", &n2);
 
     sub = n1 - n2;
 
-    printf("Sub = %d", sub);
+    printf("Resultado = %d", sub);
 
     return 0;
 }
