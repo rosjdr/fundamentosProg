f18a494542559efcb326a0c2d398e857f1ada103
Trem de Programador <rosjdr@gmail.com>
2023-04-24 17:44:26 -0300

    Nova implementação do Exercício 2 do capítulo 3

---
+++ java/src/main/java/cap3/exe2/Exercicio2.java
@@ -0,0 +1,27 @@
+package cap3.exe2;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+/**
+ * 
+ * @author rodrigo
+ * Faça um programa que receba três números, calcule e mostre a multiplicação desses números. 
+ */
+public class Exercicio2 {
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("INFORME O PRIMEIRO NÚMERO: ");
+		BigDecimal n1 = new BigDecimal(sc.nextDouble());
+		System.out.print("INFORME O SEGUNDO NÚMERO: ");
+		BigDecimal n2 = new BigDecimal(sc.nextDouble());
+		System.out.print("INFORME O TERCEIRO NÚMERO: ");
+		BigDecimal n3 = new BigDecimal(sc.nextDouble());
+		
+		BigDecimal multiplicacao = n1.multiply(n2).multiply(n3);
+		System.out.println("MULTIPLICAÇÃO = " + multiplicacao);
+		
+		
+	}
+}
